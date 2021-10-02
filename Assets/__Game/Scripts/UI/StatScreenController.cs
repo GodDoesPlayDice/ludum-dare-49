3391e56699841fb21e3ed9208c3c23a14beddc26
slowfarm <Altair08111994@yandex.ru>
2021-10-02 20:36:12 +0300

    add stat screen

---
+++ Assets/__Game/Scripts/UI/StatScreenController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatScreenController : MonoBehaviour
+{
+
+    public Text textOilCount;
+    public Text textWoodCount;
+    public Text textGoldCount;
+    public Text textAmount;
+    public Text textNews;
+
+    void Start()
+    {
+        textOilCount.text = "123";
+        textWoodCount.text = "123";
+        textGoldCount.text = "123";
+        textAmount.text = "123 $";
+        textNews.text = "News! News! News!\nNews! News! News!\nNews! News! News!";
+    }
+
+    void Update()
+    {
+        
+    }
+}
