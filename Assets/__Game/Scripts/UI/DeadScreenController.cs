7daac14f0b9d603c46a4066aa9d526d113aa8ffe
slowfarm <Altair08111994@yandex.ru>
2021-10-04 01:36:08 +0300

    dead screen added

---
+++ Assets/__Game/Scripts/UI/DeadScreenController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadScreenController : MonoBehaviour
+{
+    public void OnNewGameClick() 
+    {
+        // TODO! Start new game
+    }
+}
