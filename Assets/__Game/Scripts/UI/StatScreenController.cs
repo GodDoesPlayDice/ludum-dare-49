02867334f9e7d913d5318a5d241779c1d0ba5f31
slowfarm <Altair08111994@yandex.ru>
2021-10-03 14:09:55 +0300

    add parameters to controller

--- Assets/__Game/Scripts/UI/StatScreenController.cs
+++ Assets/__Game/Scripts/UI/StatScreenController.cs
@@ -6,19 +6,29 @@ using UnityEngine.UI;
 public class StatScreenController : MonoBehaviour
 {
 
-    public Text textOilCount;
-    public Text textWoodCount;
     public Text textGoldCount;
+    public Text textWoodCount;
+    public Text textOilCount;
     public Text textAmount;
-    public Text textNews;
+
+    public Text textGoldCurrent;
+    public Text textWoodCurrent;
+    public Text textOilCurrent;
+
+    //public Text textNews;
 
     void Start()
     {
-        textOilCount.text = "123";
-        textWoodCount.text = "123";
         textGoldCount.text = "123";
+        textWoodCount.text = "123";
+        textOilCount.text = "123";
+
         textAmount.text = "123 $";
         //textNews.text = "News! News! News!\nNews! News! News!\nNews! News! News!";
+
+        textGoldCurrent.text = "123";
+        textWoodCurrent.text = "123";
+        textOilCurrent.text = "123";
     }
 
     void Update()
