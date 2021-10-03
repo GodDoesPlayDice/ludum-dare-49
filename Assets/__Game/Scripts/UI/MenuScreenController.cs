0113f82268246cadc42c7f0dcd7f37bc453c29aa
slowfarm <Altair08111994@yandex.ru>
2021-10-03 17:14:24 +0300

    add menu screens

---
+++ Assets/__Game/Scripts/UI/MenuScreenController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreenController : MonoBehaviour
+{
+    public void OnSettingsClick() {
+        // TODO OnSettingsClick
+    }   
+
+    public void OnNewGameClick() {
+        // TODO OnnewGameClick
+    }
+
+    public void OnDescriptionClick()
+    {
+        // TODO OnDescriptionClick
+    }
+}
